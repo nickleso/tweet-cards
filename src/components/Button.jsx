09d1d65b30117c6b30c260f53cb9adda2c788bc1
Nick Leso <nickleso.work@gmail.com>
2023-05-03 19:11:34 +0300

    Try to fix hover focus on buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ function Button({ isNotFollowing, onTweetFollow }) {
   return (
     <button
       type="button"
-      className={isNotFollowing ? "tweet-button" : "tweet-button active"}
+      className={isNotFollowing ? "tweet-button" : "tweet-button-active"}
       onClick={onTweetFollow}
     >
       {isNotFollowing ? "Follow" : "Following"}
